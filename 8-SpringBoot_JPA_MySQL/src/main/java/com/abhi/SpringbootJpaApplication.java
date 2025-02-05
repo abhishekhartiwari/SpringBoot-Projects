a10eeeb2a375d762379fd69e009c34ba5d9f1330
TIWARI <abhishek.tiwari@dxc.com>
2025-02-05 20:34:29 +0530

    Springboot CRUD Operation Using JPA and MYSQL

---
+++ 8-SpringBoot_JPA_MySQL/src/main/java/com/abhi/SpringbootJpaApplication.java
@@ -0,0 +1,15 @@
+package com.abhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaApplication {
+
+	public static void main(String[] args) {
+//		ApplicationContext context = 
+				SpringApplication.run(SpringbootJpaApplication.class, args);
+//		UserRepository userRepository = context.getBean(UserRepository.class);
+	}
+
+}
